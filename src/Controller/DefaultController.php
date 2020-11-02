20e3c3754c7a1cb9aa0c1957bf94335f90d65c56
ManonDerydt <derydtmanon@gmail.com>
2020-11-02 11:07:13 +0100

    Trqvqil sur les cqtegory

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -9,11 +9,37 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class DefaultController extends AbstractController
 {
 
+    /**
+     *
+     */
     public function index()
-
     {
         # Transmettre à la vue
         return $this->render('default/index.html.twig');
+    }
+
+    public function categorie_androide()
+    {
+        # Transmettre à la vue
+        return $this->render('default/categorie_androide.html.twig');
+    }
+
+    public function categorie_accessoire()
+    {
+        # Transmettre à la vue
+        return $this->render('default/categorie_accessoire.html.twig');
+    }
+    public function fiche_produit_androide()
+
+    {
+        # Transmettre à la vue
+        return $this->render('default/fiche_produit_androide.html.twig');
+    }
+    public function fiche_produit_accessoire()
+
+    {
+        # Transmettre à la vue
+        return $this->render('default/fiche_produit_accessoire.html.twig');
     }
 
 }
