bb74a263291b50c440f12ea8028f229d8fbc8760
ManonDerydt <derydtmanon@gmail.com>
2020-11-03 15:01:29 +0100

    Categorie/ fiche_produit modif

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,6 +4,8 @@
 namespace App\Controller;
 
 
+use App\Entity\Category;
+use App\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -36,29 +38,35 @@ class DefaultController extends AbstractController
         # Transmettre à la vue
         return $this->render('default/gestion_admin.html.twig');
     }
-
-    public function categorie_androide()
+    /**
+     * @Route("/{alias}", name="default_categorie", methods={"GET"})
+     */
+    public function category($alias)
     {
+        $category = $this->getDoctrine()
+        ->getRepository(Category::class)
+        ->findOneBy(['alias' => $alias]);
+
+        $products = $category->getProducts();
         # Transmettre à la vue
-        return $this->render('default/categorie_androide.html.twig');
+        return $this->render('default/categorie.html.twig',  [
+            'products' => $products
+        ]);
     }
-
-    public function categorie_accessoire()
+    /**
+     * @Route("/{category}/{alias}_{id}.html", name="default_product", methods={"GET"})
+     */
+    public function product($id)
     {
-        # Transmettre à la vue
-        return $this->render('default/categorie_accessoire.html.twig');
-    }
-    public function fiche_produit_androide()
 
-    {
-        # Transmettre à la vue
-        return $this->render('default/fiche_produit_androide.html.twig');
-    }
-    public function fiche_produit_accessoire()
+        $product = $this->getDoctrine()
+            ->getRepository(Product::class)
+            ->find($id);
 
-    {
         # Transmettre à la vue
-        return $this->render('default/fiche_produit_accessoire.html.twig');
+        return $this->render('default/fiche_produit.html.twig',  [
+            'product' => $product
+        ]);
     }
 
 }
